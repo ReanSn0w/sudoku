d3752f28784f59e6ff230cac03281246f79fffdc
Dmitriy Papkov <papkovda@me.com>
2021-05-04 11:09:47 +0300

    Initial Commit

---
+++ sudoku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sudoku
+//
+//  Created by Дмитрий Папков on 04.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sudoku/sudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sudokuApp.swift
+//  sudoku
+//
+//  Created by Дмитрий Папков on 04.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct sudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
